43ebf335e2ed5eb589e3f07e68eafe8bb9b6fb0d
Gero <gersonhoa@gmail.com>
2022-04-18 18:28:36 -0300

    3 : valida se é um numero

--- 22.3/exercises/1/numbeis.js
+++ 22.3/exercises/1/numbeis.js
@@ -1,4 +1,5 @@
 const yourNumberIs = (a) => {
+  if (typeof a !== 'number') return 'O parâmetro deve ser um número.';
   if (a === 0 ) return 'Neutro';
 return ( a > 0 ? 'Positivo' : 'Negativo');
 }
--- 22.3/exercises/1/tests/functionresult.js
+++ 22.3/exercises/1/tests/functionresult.js
@@ -1,24 +1,39 @@
 const { expect } = require('chai');
 const numberIs = require("../numbeis")
 
-describe("Sobre Parametros", () => {
-  it('retorno é uma string', () => {
+describe("Caso o parametro seja válido", () => {
+  describe("O Retorno da função",() => {
+  it('É uma String', () => {
     expect(numberIs(1)).to.be.a('String');
   });
-})
 
-describe("Sobre retorno", () => {
-  it('retorna "Positivo" para numeros positivos', () => {
+  it('É "Positivo" para numeros positivos', () => {
     expect(numberIs(1)).to.be.equals('Positivo');
   });
 
-  it('retorna "Negativo" para numeros Negativo', () => {
+  it('É "Negativo" para numeros Negativo', () => {
     expect(numberIs(-1)).to.be.equals('Negativo');
   });
 
-  it('retorna "Neutro" para numeros Neutro', () => {
+  it('É "Neutro" para numeros Neutro', () => {
     expect(numberIs(0)).to.be.equals('Neutro');
   });
-
-  
 })
+
+})
+
+describe("Caso o parametro seja válido", () => {
+  describe("O Retorno da função", () => {
+    it('é uma "string"', () => {
+      const resposta = numberIs('AAAA');
+
+      expect(resposta).to.be.a('string');
+    });
+
+    it('é igual a "o parâmetro deve ser um número"', () => {
+      const resposta = numberIs('AAAA');
+
+      expect(resposta).to.be.equals('O parâmetro deve ser um número.');
+    });
+  });
+});
